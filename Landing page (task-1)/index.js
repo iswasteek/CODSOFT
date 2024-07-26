c8c0f4417c93dc5985523c4dc1d20d6ed6660925
Swastik Behera <swastikb97@gmail.com>
2024-07-27 01:01:56 +0530

    first landing page added

---
+++ Landing page (task-1)/index.js
@@ -0,0 +1,10 @@
+/* Toggle sidebar
+     -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+     function openNav() {
+        document.getElementById("mySidepanel").style.width = "250px";
+      }
+      
+      function closeNav() {
+        document.getElementById("mySidepanel").style.width = "0";
+      }
+      
